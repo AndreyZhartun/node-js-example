2a8a66254be9a9286decddf0851f521dc4ce2be6
Andrey Zhartun <qeetree@gmail.com>
2020-03-09 15:01:37 +0600

    added endpoint router

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express'),
      http = require('http'),
-     morgan = require('morgan');
+     morgan = require('morgan'),
+     bodyParser = require('body-parser');
 
+const endpointRouter = require('./routes/endpointRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/endpoints', endpointRouter);
+app.use('/endpoints/:epId', endpointRouter);
+
+app.get('/endpoints/:epId', (req, res, next) => {
+  res.end('Will send endpoint #' + req.params.epId);
+});
+
+app.post('/endpoints/:epId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('Not supported. Use PUT.');
+});
+
+app.put('/endpoints/:epId', (req, res, next) => {
+  res.write('Updating endpoint #' + req.params.epId);
+  res.end('Endpoint name: ' + req.body.name + '.' +
+    ' Description: ' + req.body.description);
+});
+
+app.delete('/endpoints/:epId', (req, res, next) => {
+  res.end('Deleting endpoint #' + req.params.epId);
+});
 
 app.use(express.static(__dirname + '/public'));
 
